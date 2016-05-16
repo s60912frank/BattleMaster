fd0e9bdf400c0d62339ac07b07147dec659e85c7
s60912frank <s60912frank@gmail.com>
2016-05-16 16:07:52 +0800

    把大部分遊戲畫面改成直的

---
+++ Client/Assets/MonsterEntry.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterEntry : MonoBehaviour {
+    public enum Direction
+    {
+        ToLeft,
+        ToRight
+    }
+    public Direction dir = Direction.ToLeft;
+    public float speed = 0.2f;
+    public float distance = 50;
+    private Vector3 finalPos;
+	// Use this for initialization
+	void Start () {
+        finalPos = transform.position;
+        if (dir == Direction.ToLeft)
+            transform.position += distance * Vector3.right;
+        else if(dir == Direction.ToRight)
+            transform.position -= distance * Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dir == Direction.ToLeft && transform.position.x - finalPos.x > 0)
+            transform.position -= speed * Vector3.right;
+        else if (dir == Direction.ToRight && transform.position.x - finalPos.x < 0)
+            transform.position += speed * Vector3.right;
+	}
+}
--- Client/Assets/PartnerSkillEffectEntry.cs
+++ Client/Assets/PartnerSkillEffectEntry.cs
@@ -1,39 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PartnerSkillEffectEntry : MonoBehaviour {
 
     public bool activated;
+    private RectTransform rt;
     private Vector3 startPos;
     private Vector3 startScale;//save initial states
 	// Use this for initialization
 	void Start () {
+        rt  = gameObject.GetComponent<RectTransform>();
         activated = false;
-        startPos = transform.position;
-        startScale = transform.localScale;
+        startPos = rt.anchoredPosition;
+        startScale = rt.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.x >= 20 || transform.position.x <= -20)//set activate to false if out of screen range(roughly) and enable the else loop below to run
+        if (rt.anchoredPosition.x >= 800 || rt.anchoredPosition.x <= -800)//set activate to false if out of screen range(roughly) and enable the else loop below to run
             activated = false;
         if (activated)//run animate
         {
-            if (transform.position.x >= 1){
-                transform.Translate(-1, 0.08f, 0);
-                transform.localScale += new Vector3(0.1f, 0.1f, 0);
+            if (rt.position.x >= 30){
+                rt.Translate(-10, 0.08f, 0);
+                rt.localScale += new Vector3(0.1f, 0.1f, 0);
             }
-            else if (transform.position.x <= -1){
-                transform.Translate(-1, -0.08f, 0);
-                transform.localScale += new Vector3(-0.1f, -0.1f, 0);
+            else if (rt.position.x <= -30){
+                rt.Translate(-10, -0.08f, 0);
+                rt.localScale += new Vector3(-0.1f, -0.1f, 0);
             }
             else//middle  display
-                transform.Translate(-0.01f, 0, 0);
+                transform.Translate(-1, 0, 0);
         }
         else//set position & scale to initial state
         {
-            transform.position = startPos;
-            transform.localScale = startScale;
+            rt.anchoredPosition = startPos;
+            rt.localScale = startScale;
         }
 	}
 }
