e876102b2a87a9e86beb04bf2ce02dff631a813f
s60912frank <s60912frank@gmail.com>
2016-07-24 23:28:56 +0800

    初步增加選角場景

---
+++ Client/Assets/SelectPartner/TestRotation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRotation : MonoBehaviour {
+    private float rotateDelta = 0;
+    private bool isRotating = false;
+    private Vector3 CylinderAxis;
+	// Use this for initialization
+	void Start () {
+        CylinderAxis = Quaternion.AngleAxis(-15, Vector3.up).eulerAngles;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(CylinderAxis, rotateDelta);
+    }
+
+    public void TurnLeft()
+    {
+        if (!isRotating)
+        {
+            StartCoroutine(SmoothRotate(-120));
+        }
+    }
+
+    public void TurnRight()
+    {
+        if (!isRotating)
+        {
+            StartCoroutine(SmoothRotate(120));
+        }
+    }
+
+    private IEnumerator SmoothRotate(int angle)
+    {
+        isRotating = true;
+        int counter = Mathf.Abs(angle);
+        while(counter > 0)
+        {
+            rotateDelta = angle > 0 ? 4 : -4;
+            counter -= 4;
+            yield return new WaitForEndOfFrame();
+        }
+        rotateDelta = 0;
+        isRotating = false;
+        Debug.Log("WHEE");
+    }
+}
